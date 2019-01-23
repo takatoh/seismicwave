79541beca3a9914bfeb2b60b20003781a2c638c6
takatoh <takatoh.m@gmail.com>
2019-01-23 10:10:58 +0900

    cmd/example3/main.go: New.

---
+++ cmd/example3/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/takatoh/seismicwave"
+)
+
+func main() {
+	opt_name := flag.String("wavename", "", "Specify wave name.")
+	opt_format := flag.String("format", "", "Wave format.")
+	opt_dt := flag.Float64("dt", 0.0, "dt.")
+	opt_ndata := flag.Int("ndata", 0, "Number of data.")
+	opt_skip := flag.Int("skip", 0, "Skip lines.")
+	flag.Parse()
+
+	filename := flag.Args()[0]
+
+	wave, err := seismicwave.LoadFixedFormat(
+		filename,
+		*opt_name,
+		*opt_format,
+		*opt_dt,
+		*opt_ndata,
+		*opt_skip,
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Name     = %s\n", wave.Name)
+	fmt.Printf("Dt       = %f\n", wave.Dt)
+	fmt.Printf("Max.     = %f\n", wave.Max())
+	fmt.Printf("Min.     = %f\n", wave.Min())
+	fmt.Printf("Abs.Max. = %f\n", wave.AbsMax())
+	fmt.Printf("NData    = %d\n", wave.NData())
+	fmt.Printf("Length   = %f sec\n", wave.Length())
+}
